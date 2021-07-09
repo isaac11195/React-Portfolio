71bdc100cb527a7b3d8abad39fbeb5e36fc3e293
Mona Lisa <isaac11195@gmail.com>
2021-07-08 22:56:38 -0400

    add new commands

---
+++ react-portfolio/src/resumeData.js
@@ -0,0 +1,99 @@
+let resumeData = {
+    "name": "Isaac Perez",
+    "role": "Frontend Developer and Data Scientist",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/isaac-perez-35965b12b/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/isaac11195",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Florida",
+    "education":[
+      {
+        "UniversityName":"UCF",
+        "specialization":"Coding Development",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2021",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Caribbean University",
+        "specialization":"Electrical Engineering",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2013",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2020",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname": "PLC"
+      },
+      {
+        "skillname": "Java"
+      },
+      {
+        "skillname": "Power"
+      }
+    ],
+    "portfolio":[
+      
+      {
+        "name":"project1",
+        "description":"lyricsong",
+        "imgurl":"images/portfolio/lyric.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"rentaldog",
+        "imgurl":"images/portfolio/rentalpuppy.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
